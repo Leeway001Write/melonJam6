3401724483738d98bc3d1ff5415c81ab0fd8ee5d
CalebDew <flameofgondor@gmail.com>
2025-07-25 17:25:01 -0600

    You also saw what I did then.

--- escape-from-greenland/Scripts/ship.gd
+++ escape-from-greenland/Scripts/ship.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-var speed:float = 0
+@export var turn_speed:float = 12
+
+#var speed:float = 0
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed('forward'):
 		for booster:Booster in $Components/Boosters.get_children():
-			speed += booster.speed
-		#print_debug(speed)
+			var child_vector = -Vector2(booster.global_position - global_position).normalized()
+			child_vector *= booster.speed
+			velocity += child_vector
 	
-	var dir = Vector2($ForwardPos.global_position - global_position).normalized()
-	velocity = dir * speed
+	#var dir = Vector2($ForwardPos.global_position - global_position).normalized()
+	
+	var turn_axis = Input.get_axis('left', 'right')
+	rotate(deg_to_rad(turn_speed * turn_axis))
 	move_and_slide()
