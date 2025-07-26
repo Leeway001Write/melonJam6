a9b743e45bc1540686dc224b5811ba53ee989c2e
CalebDew <flameofgondor@gmail.com>
2025-07-26 15:40:08 -0600

    Made a score indicator that doesn't work yet

---
+++ escape-from-greenland/Scripts/score.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var cam = get_tree().get_first_node_in_group('Cam')
+
+var score = 0
+
+func _process(delta: float) -> void:
+	global_position = cam.global_position
+	
+func increase_score(amount:int):
+	score += amount
+	$AnimCont/Score.text = "Score: " + score
+	$AnimationPlayer.play('Score')
