8703ed0db2d67b71711f99b6a35a1f45e04fb34c
aaevans03 <aaevans03@gmail.com>
2025-07-25 20:33:29 -0600

    Implemented destroying of compounded components

--- escape-from-greenland/Scripts/booster.gd
+++ escape-from-greenland/Scripts/booster.gd
@@ -7,3 +7,5 @@ var dir_vector = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	dir_vector = Vector2($ForwardPos.global_position - global_position).normalized() * speed
+	if test && Input.is_action_just_pressed("DestroyTest"):
+		_hurt()
--- escape-from-greenland/Scripts/component.gd
+++ escape-from-greenland/Scripts/component.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 class_name Component
 
 @export var attached:bool
+@export var health:int
+@export var test:bool
 @onready var trigger:Area2D = $Area2D
 
-var attached_items=[]
 signal hit_something
+signal destroyed
 
 #func _ready() -> void:
 	#if attached:
@@ -18,10 +20,23 @@ signal hit_something
 		#trigger.get_child(0).disabled = true
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if not attached:
-		return
-	print_debug(area.get_parent().name)
 	if area.is_in_group("Component"):
-		var component = area.get_parent()
+		var component:Component = area.get_parent()
+		if component.attached:
+			if not component.destroyed.is_connected(destroy):
+				component.destroyed.connect(destroy)
+		if not attached:
+			return
 		hit_something.emit(component, area)
-		attached_items.append(component)
+	elif area.is_in_group("DamagePlayer"):
+		_hurt()
+
+func _hurt():
+	health -= 1
+	if health <= 0:
+		destroy()
+	
+
+func destroy():
+	destroyed.emit()
+	queue_free()
