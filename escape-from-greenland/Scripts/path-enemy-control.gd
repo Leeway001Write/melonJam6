1471b13ba0bf2d8cafc6c20a4040692f651bc822
aaevans03 <aaevans03@gmail.com>
2025-07-26 23:18:18 -0600

    Fixed enemy bullet firing

--- escape-from-greenland/Scripts/path-enemy-control.gd
+++ escape-from-greenland/Scripts/path-enemy-control.gd
@@ -11,6 +11,7 @@ extends Path2D
 
 var firing = true
 var enemy_dead = false
+var fire_from_left = true
 
 func _ready() -> void:
 	enemy_ship.died.connect(_enemy_die)
@@ -29,20 +30,26 @@ func _physics_process(delta: float) -> void:
 	firing = enemy_dist < max_dist
 	
 	enemy_ship.look_at(player.global_position)
-	
-
-
 
 func _on_timer_timeout() -> void:
 	if enemy_dead: return
 	if firing:
 		var bullet_inst = bullet.instantiate()
-		bullet_inst.global_position = enemy_ship.global_position
+		
+		# Determine which arm to fire bullet from
+		if fire_from_left:
+			bullet_inst.global_position = $Enemy/LeftArm.global_position
+			fire_from_left = false
+		else:
+			bullet_inst.global_position = $Enemy/RightArm.global_position
+			fire_from_left = true
 		
 		# Calculate the direction of the bullets
 		var bullet_vector = Vector2(player.global_position - enemy_ship.global_position).normalized()
+		
 		# Remove the bullet from the wrong class and put it in the right class
 		bullet_inst.find_child('Area2D').remove_from_group('PlayerBullet')
 		bullet_inst.find_child('Area2D').add_to_group('DamagePlayer')
 		bullet_inst.velocity = bullet_vector * bullet_speed
+		bullet_inst.global_rotation = atan2(bullet_vector.y, bullet_vector.x)
 		get_tree().root.add_child.call_deferred(bullet_inst)
