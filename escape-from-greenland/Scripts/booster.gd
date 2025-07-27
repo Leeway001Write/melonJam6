bde10675fb46678324670fd39dfb20e43cdcf790
CalebDew <flameofgondor@gmail.com>
2025-07-26 20:17:21 -0600

    Fixed initial booster glitch and made the lighting system

--- escape-from-greenland/Scripts/booster.gd
+++ escape-from-greenland/Scripts/booster.gd
@@ -11,6 +11,8 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed('forward') and attached:
 		$CPUParticles2D.emitting = true
+		if initial:
+			$PointLight2D.enabled = true
 		$Fire.play('burst')
 		await get_tree().create_timer(.5).timeout
 		if Input.is_action_pressed('forward'):
@@ -19,9 +21,12 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_released('forward') and attached:
 		$CPUParticles2D.emitting = false
+		$PointLight2D.enabled = false
 		$Fire.play('burst', -1, true)
 		
 	if Input.is_action_pressed('forward') and not $Fire.is_playing() and attached:
 		$Fire.play("burn")
+		if initial:
+			$PointLight2D.enabled = true
 		$CPUParticles2D.emitting = true
 		
--- escape-from-greenland/Scripts/bullet.gd
+++ escape-from-greenland/Scripts/bullet.gd
@@ -6,7 +6,11 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if $Area2D.is_in_group("DamagePlayer"):
-		if area.is_in_group('Component') or area.is_in_group('Player'):
+		if area.is_in_group('Component'):
+			if area.get_parent().initial: return
+			await get_tree().physics_frame
+			queue_free()
+		elif area.is_in_group('Player'):
 			await get_tree().physics_frame
 			queue_free()
 	elif $Area2D.is_in_group('PlayerBullet'):
